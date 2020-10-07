0fdae1df9ff141fb1176987c83426d400c41fba4
Joachim <joachim.toefting@gmail.com>
2020-10-07 18:55:31 +0200

    Price added

--- DataLayer/Entities/Product/Product.cs
+++ DataLayer/Entities/Product/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DataLayer.Entities
@@ -8,6 +9,8 @@ namespace DataLayer.Entities
 	{
 		public int ProductID { get; set; }
 		public string Name { get; set; }
+		[Column(TypeName ="decimal(19,2)")]
+		public double Price { get; set; }
 		public int? BrandID { get; set; }
 		public Brand Brand { get; set; }
 		public ICollection<ProductTag> ProductTags { get; set; }
