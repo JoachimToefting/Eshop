d9ad7fd5e9fb36f017732762ab595fe02c86f853
Joachim Steen Tøfting <joac3109@elevcampus.dk>
2020-10-22 21:30:41 +0200

    adminpanel 2.0

--- EshopWebApp/Pages/Admin/AdminPanel.cshtml.cs
+++ EshopWebApp/Pages/Admin/AdminPanel.cshtml.cs
@@ -4,14 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServiceLayer.BrandService;
+using ServiceLayer.ProductService;
+using ServiceLayer.ProductService.Concrete;
 
 namespace EshopWebApp.Pages.Admin
 {
     public class AdminPanelModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IListProductService _listProductService;
+		public AdminPanelModel(IListProductService listProductService)
+		{
+            _listProductService = listProductService;
+		}
+		[BindProperty(SupportsGet = true)]
+		public int? pageCurrent { get; set; }
 
-        }
+		[BindProperty(SupportsGet = true)]
+		public int? pageSize { get; set; }
+		[BindProperty]
+		public int totalPages { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string searchTerm { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public int? orderBy { get; set; }
+		public IList<ProductListDto> Products { get; set; }
+		public async Task OnGetAsync()
+        {
+            ProductFilterSortPageOptions productFilterSortPageOptions = new ProductFilterSortPageOptions();
+			if (!string.IsNullOrEmpty(searchTerm))
+			{
+				productFilterSortPageOptions.FilterBy = ServiceLayer.ProductService.QueryObjects.ProductFilterBy.ByLikeAll;
+				productFilterSortPageOptions.FilterValue = searchTerm;
+			}
+			if (orderBy.HasValue)
+			{
+				productFilterSortPageOptions.OrderBy = (ServiceLayer.ProductService.QueryObjects.OrderByOptions)orderBy;
+			}
+			if (pageSize.HasValue)
+			{
+				productFilterSortPageOptions.PageSize = (int)pageSize;
+			}
+			if (pageCurrent.HasValue)
+			{
+				productFilterSortPageOptions.PageNum = (int)pageCurrent;
+			}
+			Products = await _listProductService.FilterSortPage(productFilterSortPageOptions).ToListAsync();
+			totalPages = productFilterSortPageOptions.NumPages;
+		}
     }
 }
