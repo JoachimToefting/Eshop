92ccad4e42c4070f24f55efb928b112b524b9645
Joachim Steen Tøfting <joac3109@elevcampus.dk>
2020-10-26 16:27:32 +0100

    on delete product delete img

--- EshopWebApp/Pages/Admin/ProductPages/DeleteProduct.cshtml.cs
+++ EshopWebApp/Pages/Admin/ProductPages/DeleteProduct.cshtml.cs
@@ -9,16 +9,24 @@ using DataLayer;
 using DataLayer.Entities;
 using ServiceLayer.ProductService.Concrete;
 using ServiceLayer.ProductService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace EshopWebApp.Pages.Admin.ProductPages
 {
 	public class DeleteProductModel : PageModel
 	{
 		private readonly IListProductService _listProductService;
+		private readonly IWebHostEnvironment _webHostEnvironment;
+		private readonly ILogger<EditProductModel> _logger;
 
-		public DeleteProductModel(IListProductService listProductService)
+		public DeleteProductModel(ILogger<EditProductModel> logger, IListProductService listProductService, IWebHostEnvironment webHostEnvironment)
 		{
+			_logger = logger;
 			_listProductService = listProductService;
+			_webHostEnvironment = webHostEnvironment;
+			
 		}
 
 		[BindProperty]
@@ -49,6 +57,19 @@ namespace EshopWebApp.Pages.Admin.ProductPages
 				return NotFound();
 			}
 
+			if (!string.IsNullOrEmpty(Product.ImgPath))
+			{
+				string filepath = Path.Combine(_webHostEnvironment.WebRootPath, "img", $"{Product.ProductID}.{Product.ImgPath}");
+				if (System.IO.File.Exists(filepath))
+				{
+					//No need to update product as it will be deleted
+					System.IO.File.Delete(filepath);
+				}
+				else
+				{
+					_logger.LogWarning("No file was deleted and has to be deleted manual");
+				}
+			}
 
 			await _listProductService.DeleteByIdAsync((int)id);
 
